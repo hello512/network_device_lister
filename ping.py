9c6c48f51adaae6c45291e0a4ce22df031982622
hello512 <debian-user@debian.debian>
2021-05-29 21:26:17 +0200

    it now send a message

--- ping.py
+++ ping.py
@@ -7,6 +7,24 @@ SOCK = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP)
 
 host = socket.gethostbyname(socket.gethostname())
 
+
+class ICMPHeader:
+    def __init__(self, typ=8, code=0, checksum=False):
+        self.type = typ
+        self.code = code
+        self.checksum = self.calc_checksum() if not checksum else checksum
+    
+
+    def calc_checksum(self):
+        #this function is wrong :D
+#        block = (self.type << 8)|self.code
+        return 1236#~block
+
+    def get_bytes(self):
+        print(self.checksum & 0xffffffff)
+        return self.type.to_bytes(1, "big") + self.code.to_bytes(1, "big") + self.checksum.to_bytes(2, "big")
+    
+    
 def ping():
     pass
 
@@ -21,5 +39,15 @@ if __name__ == "__main__":
     print("socket closed")
     print(socket.gethostbyname(socket.gethostname()))
 
+    header = ICMPHeader()
+
     host = socket.gethostbyname(socket.gethostname())
-    SOCK.bind((host, 0))
+    print("host: ", host)
+    SOCK.bind(("192.168.0.218", 0))
+
+    #message = b"hello world"
+
+
+    target = "192.168.0.1"
+
+    SOCK.sendto(header.get_bytes(), (target, 1))
