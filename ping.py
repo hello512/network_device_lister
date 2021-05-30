e748e9700dab16f49bd11e2abe4202c4d31be65d
hello512 <debian-user@debian.debian>
2021-05-30 12:12:08 +0200

    checksum is still wrong

--- ping.py
+++ ping.py
@@ -1,5 +1,7 @@
 import sys
+import time
 import socket
+import random
 
 ICMP = socket.getprotobyname("icmp")
 
@@ -23,8 +25,19 @@ class ICMPHeader:
     def get_bytes(self):
         print(self.checksum & 0xffffffff)
         return self.type.to_bytes(1, "big") + self.code.to_bytes(1, "big") + self.checksum.to_bytes(2, "big")
-    
-    
+   
+class ICMPMessage:
+    def __init__(self, header):
+        self.header = header.get_bytes()
+        self.timestamp = int(time.time())
+        self.identifier = random.randint(0, 2**15 - 1)
+        self.sequence_num = random.randint(0, 2**15 - 1)
+        self.data = b"hello world"
+
+    def getbmessage(self):
+        print(self.timestamp)
+        return self.header + self.identifier.to_bytes(2, "big") + self.sequence_num.to_bytes(2, "big") + self.timestamp.to_bytes(4, "big") + self.data
+
 def ping():
     pass
 
@@ -40,6 +53,7 @@ if __name__ == "__main__":
     print(socket.gethostbyname(socket.gethostname()))
 
     header = ICMPHeader()
+    message = ICMPMessage(header)
 
     host = socket.gethostbyname(socket.gethostname())
     print("host: ", host)
@@ -50,4 +64,4 @@ if __name__ == "__main__":
 
     target = "192.168.0.1"
 
-    SOCK.sendto(header.get_bytes(), (target, 1))
+    SOCK.sendto(message.getbmessage(), (target, 1))
