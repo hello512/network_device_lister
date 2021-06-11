8392c26c92f7c8d826297efc922494e79bbb08c7
hello512 <anton.anderten@me.com>
2021-06-11 11:18:32 +0200

    removed some stuff

--- network_device_lister.py
+++ network_device_lister.py
@@ -10,20 +10,6 @@ import threading
 CONNECTIONS = []
 MSGS = []
 
-def ping(ip):
-	con = icmp.ICMPConnection()
-	header = icmp.ICMPHeader()
-	msg = icmp.ICMPMessage(header)
-	MSGS.append(msg.getbmessage())
-	#try:
-	con.send(ip, msg)
-	rm = con.recv(1)#.getbmessage())
-	#print(rm)
-	if rm.getbmessage() in MSGS:
-		print(ip, "worked")
-	#except:
-	#	print(ip, "not working")
-
 #for testing purposes: ip range from 0.0 to 0.255
 def ping_all():
 	t0 = time.time()
@@ -52,12 +38,3 @@ def ping_all():
 
 if __name__ == "__main__":
 	ping_all()
-	#print(MSGS)
-"""
-	con = icmp.ICMPConnection()
-	msg = icmp.ICMPMessage()
-	print(msg.getbmessage())
-	con.send("192.168.0.1", msg)
-	rm = con.recv(1)
-	print(rm.getbmessage())
-"""
