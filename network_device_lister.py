f9c77261b8fe34995e4b3e8f1613774458084e25
hello512 <anton.anderten@me.com>
2021-06-18 13:30:59 +0200

    now has ip range

--- network_device_lister.py
+++ network_device_lister.py
@@ -11,15 +11,20 @@ CONNECTIONS = []
 MSGS = []
 
 #for testing purposes: ip range from 0.0 to 0.255
-def ping_all():
+def ping_all(args):
 	t0 = time.time()
 	con = icmp.ICMPConnection()
-	for i in range(0, 256):
-		header = icmp.ICMPHeader()
-		msg = icmp.ICMPMessage(header)
-		MSGS.append((msg, "192.168.0." + str(i)))
-		#try:
-		con.send("192.168.0." + str(i), msg)
+
+	args.splt()
+	for i0 in range(args.start_ip[0], args.end_ip[0] + 1):
+		for i1 in range(args.start_ip[1], args.end_ip[1] + 1):
+			for i2 in range(args.start_ip[2], args.end_ip[2] + 1):
+				for i3 in range(args.start_ip[3], args.end_ip[3] + 1):
+					header = icmp.ICMPHeader()
+					msg = icmp.ICMPMessage(header)
+					ip = str(i0) + "." + str(i1) + "." + str(i2) + "." + str(i3)
+					MSGS.append((msg, ip))
+					con.send(ip, msg)
 
 	while True:
 		rm = con.recv(1)
@@ -31,7 +36,34 @@ def ping_all():
 				print(m[1], "	answered in: ", time.time() - rm.timestamp, "	seconds")
 
 
+class Args:
+	def __init__(self, file_name="", start_ip="192.168.0.1", end_ip="192.168.0.255"):
+		self.file_name = file_name
+		self.sip = start_ip
+		self.eip = end_ip
+		self.start_ip = [int(_) for _ in self.sip.split(".")]
+		self.end_ip = [int(_) for _ in self.eip.split(".")]
+
+	def splt(self):
+		self.start_ip = [int(_) for _ in self.sip.split(".")]
+		self.end_ip = [int(_) for _ in self.eip.split(".")]
+
+
+def analyse_args():
+	args = Args()
+	for i, a in enumerate(sys.argv):
+		if a == "-s":
+			args.sip = sys.argv[i + 1]
+
+		if a == "-e":
+			args.eip = sys.argv[i + 1]
+
+		if a == "-f":
+			args.file_name = sys.argv[i + 1]
+
+	return args
 
 
 if __name__ == "__main__":
-	ping_all()
+	args = analyse_args()
+	ping_all(args)
